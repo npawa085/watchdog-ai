2d9696bea2a3679efa39326ca3e7645147f2760f
CPG-GPC\Neha.Pawar <neha.pawar@purolator.com>
2022-04-07 22:51:29 -0400

    smallpotatoes

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,30 +1,30 @@
-const { validationResult } = require("express-validator");
-const HttpError = require("../models/http-error");
+// const { validationResult } = require("express-validator");
+// const HttpError = require("../models/http-error");
 let { PythonShell } = require("python-shell");
 
 const currDB = [
   {
-    id: "1",
+    id: 1,
     username: "A B",
   },
   {
-    id: "2",
+    id: 2,
     username: "C D",
   },
   {
-    id: "3",
+    id: 3,
     username: "E F",
   },
   {
-    id: "4",
+    id: 4,
     username: "G H",
   },
   {
-    id: "5",
+    id: 5,
     username: "I J",
   },
   {
-    id: "6",
+    id: 6,
     username: "K L",
   },
 ];
@@ -44,53 +44,100 @@ const getUsers = (req, res, next) => {
     newArr[i] = currDB[i].username;
   }
 
-  res.send(newArr)
+  //res.send([newArr]);
   return newArr;
 };
 
-const add = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
-  }
-  const { username } = req.body;
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
+function one() {
   let options = {
-    args: [nextId()]
+    args: [nextId()],
   };
-  console.log("hi");
-  // setTimeout(
-    PythonShell.run("01_face_dataset.py", options, function (err, result) {
-      if (err) throw err;
-      console.log(result);
-    });
+  PythonShell.run("01_face_dataset.py", options, function (err, result) {
+    if (err) throw err;
+    console.log(result);
     console.log("hello");
-  // ),
-  //   7000;
+  });
+}
+function two() {
+  PythonShell.run("02_face_training.py", null, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    console.log("hi");
+  });
+}
+function three() {
+  let options2 = {
+    args: [getUsers()],
+  };
+  PythonShell.run("03_face_recognition.py", options2, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+async function four() {
+  one();
+  await sleep(7000);
+  two();
+  await sleep(2000);
+  three();
+}
+
+const add = (req, res, next) => {
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   console.log(req);
+  //   throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  // }
+
+  const { username } = req.body;
+  // let options = {
+  //   args: [nextId()]
+  // };
+  //console.log("hi");
+
+  // PythonShell.run("01_face_dataset.py", options, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  //   console.log("hello");
+  // });
+  const newArr = [];
+  for (var i = 0; i < currDB.length; i++) {
+    newArr[i] = currDB[i].username;
+  }
+  console.log(newArr);
+  console.log(getUsers());
+  four();
+
   const createdUser = {
     id: nextId(),
     username,
   };
 
+  console.log(currDB);
   currDB.push(createdUser);
   console.log(createdUser);
+  console.log(currDB);
 
-  setTimeout(
-    PythonShell.run("02_face_training.py", null, function (err, result) {
-      if (err) throw err;
-      console.log(result);
-    })
-  ),
-    2000;
+  // PythonShell.run("02_face_training.py", null, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  //   console.log("hi");
+  // });
 
-  let options2 = {
-    args: [getUsers()]
-  };
+  // let options2 = {
+  //   args: [getUsers()]
+  // };
 
-  PythonShell.run("03_face_recognition.py", options2, function (err, result) {
-    if (err) throw err;
-    console.log(result);
-  });
+  // PythonShell.run("03_face_recognition.py", options2, function (err, result) {
+  //   if (err) throw err;
+  //   console.log(result);
+  // });
 
   res.json({ user: createdUser });
 };
